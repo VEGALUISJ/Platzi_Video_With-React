3546ffac0cfde3737ea607983383de193fe69b9a
Jose Vega <luisvega.jlvv@gmail.com>
2020-05-15 11:20:44 -0400

    ESlint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => {
+    return (
+        <h1>Hola Mundo de prueba</h1>
+    )
+};
+
+
+export default HelloWorld;
